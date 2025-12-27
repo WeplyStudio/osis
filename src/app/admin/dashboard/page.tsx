3f48f7687ee8fd7fc685587f7db5dfcaf1e85b2d
Steward Jason <matchboxdevelopment@gmail.com>
2025-12-27 01:38:01 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ClientDashboardPage from './ClientDashboardPage';
-import { initializeFirebase } from '@/firebase';
-import { getDocs, collection, doc, getDoc } from 'firebase/firestore';
-import type { TeamMember } from '@/lib/types';
 import { unstable_noStore as noStore } from 'next/cache';
 
 
-export default async function DashboardPage() {
+export default function DashboardPage() {
   noStore();
-  const { firestore } = initializeFirebase();
-  
-  const teamMembersCollection = collection(firestore, 'teamMembers');
-  const teamMembersSnapshot = await getDocs(teamMembersCollection);
-  const teamMembers = teamMembersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as TeamMember));
 
-  const aboutUsDoc = doc(firestore, 'siteContent', 'aboutUs');
-  const aboutUsSnapshot = await getDoc(aboutUsDoc);
-const aboutUsImage = aboutUsSnapshot.data() as { url: string, hint: string };
-
-  return <ClientDashboardPage teamMembers={teamMembers} aboutUsImage={aboutUsImage} />;
+  return <ClientDashboardPage />;
 }
