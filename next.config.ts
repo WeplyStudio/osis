651aa86325ef02b019120ae9d224b5a824416b71
Steward Jason <matchboxdevelopment@gmail.com>
2025-12-27 00:58:29 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://i.ibb.c

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'encrypted-tbn0.gstatic.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
